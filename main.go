0595ff16d88eb0d3f4bc24cc78e12a46e03d9559
jackellisw <jackwellis0@gmail.com>
2025-11-12 21:53:57 +0000

    starting point

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"log"
+	"os"
+)
+
 func main() {
+	reader, err := os.Open("testdata/chuck.jpg")
+	if err != nil {
+		log.Fatalf("Could not open file: %s", err)
+	}
+	defer reader.Close()
+
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatalf("Could not decode image: %s", err)
+	}
+
+	bounds := m.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y += 20 {
+		for x := bounds.Min.X; x < bounds.Max.X; x += 20 {
+			// get all colors from image
+			r, g, b, a := m.At(x, y).RGBA()
+
+			combined := (0.2126 * float64(r)) + (0.7152 * float64(g)) + (0.0722*float64(b))/257
+			fmt.Println(combined)
+			fmt.Println(a)
+
+		}
+	}
 }
