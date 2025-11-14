bfe2171b9617b3a2f269fe3237d32590e9814301
jackellisw <jackwellis0@gmail.com>
2025-11-14 15:30:14 +0000

    finished (mapped brightness to index and index to char and outputted to a .txt

--- main.go
+++ main.go
@@ -20,16 +20,28 @@ func main() {
 		log.Fatalf("Could not decode image: %s", err)
 	}
 
-	bounds := m.Bounds()
-	for y := bounds.Min.Y; y < bounds.Max.Y; y += 20 {
-		for x := bounds.Min.X; x < bounds.Max.X; x += 20 {
-			// get all colors from image
-			r, g, b, a := m.At(x, y).RGBA()
+	outputFile, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatalf("Could not create file")
+	}
 
-			combined := (0.2126 * float64(r)) + (0.7152 * float64(g)) + (0.0722*float64(b))/257
-			fmt.Println(combined)
-			fmt.Println(a)
+	defer outputFile.Close()
+
+	asciiBrightnessChars := "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^."
+
+	bounds := m.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y += 40 {
+		for x := bounds.Min.X; x < bounds.Max.X; x += 10 {
+			r, g, b, _ := m.At(x, y).RGBA()
+
+			combined := ((0.2126 * float64(r)) + (0.7152 * float64(g)) + (0.0722 * float64(b))) / 257
+
+			resultIndex := int((combined / 255) * float64(len(asciiBrightnessChars)-1))
+			outputFile.WriteString(string(asciiBrightnessChars[resultIndex]))
 
 		}
+		outputFile.WriteString("\n")
 	}
+
+	fmt.Println("Saved to output.txt")
 }
